42f145e334e175c2617a0868f828b06205bd8e7f
Vadym Vynokur <king.krollie@gmail.com>
2025-12-27 15:52:37 +0200

    add solution

--- cinema/views.py
+++ cinema/views.py
@@ -12,12 +12,12 @@ def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = MovieSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
